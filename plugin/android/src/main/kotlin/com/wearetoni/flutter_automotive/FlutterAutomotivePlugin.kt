dcc05a6aaa86cf4671ebe193eb30b4e5bcfca9b8
Johann Schramm <43448334+JohannSchramm@users.noreply.github.com>
2025-05-06 12:04:01 +0200

    feat: Implement android car platform code

--- plugin/android/src/main/kotlin/com/wearetoni/flutter_automotive/FlutterAutomotivePlugin.kt
+++ plugin/android/src/main/kotlin/com/wearetoni/flutter_automotive/FlutterAutomotivePlugin.kt
@@ -1,33 +1,49 @@
 package com.wearetoni.flutter_automotive
 
+import android.car.Car
+import android.car.hardware.CarPropertyValue.STATUS_AVAILABLE
+import android.car.hardware.property.CarPropertyManager
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 
 /** FlutterAutomotivePlugin */
-class FlutterAutomotivePlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class FlutterAutomotivePlugin: FlutterPlugin, FlutterAutomotiveApi {
+  private lateinit var propertyManager: CarPropertyManager;
+
+  // --- Setup ---
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_automotive")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
+    FlutterAutomotiveApi.setUp(flutterPluginBinding.binaryMessenger, this)
+    val car = Car.createCar(flutterPluginBinding.applicationContext)
+    propertyManager = car.getCarManager(Car.PROPERTY_SERVICE) as CarPropertyManager
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+    FlutterAutomotiveApi.setUp(binding.binaryMessenger, null)
+  }
+
+  // --- Methods ---
+
+  override fun getProperty(
+    propertyId: Long,
+    areaId: Long,
+    callback: (kotlin.Result<Any?>) -> Unit
+  ) {
+    val prop = propertyManager.getProperty<Any>(propertyId.toInt(), areaId.toInt())
+    if (prop.propertyStatus == STATUS_AVAILABLE) {
+      callback(Result.success(prop.value));
+    } else {
+      callback(Result.failure(Exception("Property not available")))
+    }
+  }
+
+  override fun setProperty(
+    propertyId: Long,
+    areaId: Long,
+    value: Any?,
+    callback: (kotlin.Result<Unit>) -> Unit
+  ) {
+    // TODO test if the class property works
+    propertyManager.setProperty(value?.javaClass, propertyId.toInt(), areaId.toInt(), value)
+    callback(Result.success(Unit))
   }
 }
