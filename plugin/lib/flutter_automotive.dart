59913d32c8b8dff9b1c60b678f0d9a978a182ab9
Johann Schramm <43448334+JohannSchramm@users.noreply.github.com>
2025-05-06 11:14:30 +0200

    feat: Implement main FlutterAutomotive class

--- plugin/lib/flutter_automotive.dart
+++ plugin/lib/flutter_automotive.dart
@@ -1,1 +1,6 @@
-class FlutterAutomotive {}
+import 'package:flutter_automotive/src/vehicle_datasource_impl.dart';
+import 'package:flutter_automotive/src/vehicle_repository.dart';
+
+class FlutterAutomotive extends VehiclePropertyRepository {
+  FlutterAutomotive() : super(VehiclePropertyDatasourceImpl());
+}
---
+++ plugin/lib/src/vehicle_datasource_impl.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: non_constant_identifier_names, unnecessary_type_check
+library;
+
+import 'package:flutter_automotive/src/flutter_automotive_platform_interface.dart';
+import 'package:flutter_automotive/src/vehicle_datasource.dart';
+
+class VehiclePropertyDatasourceImpl extends VehiclePropertyDatasource {
+  VehiclePropertyDatasourceImpl({FlutterAutomotivePlatform? instance}) : 
+    _platform = instance ?? FlutterAutomotivePlatform.instance;
+
+  final FlutterAutomotivePlatform _platform;
+
+  @override
+  Future<dynamic> getProperty(int propertyId, int areaId) async {
+    return await _platform.getProperty(propertyId, areaId);
+  }
+
+  @override
+  Future<void> setProperty(int propertyId, int areaId, value) async {
+    return await _platform.setProperty(propertyId, areaId, value);
+  }
+}
